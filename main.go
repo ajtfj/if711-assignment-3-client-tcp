7a5a0bc5fd0af0eb49464768714ab1b4ca2b446d
Almir Menezes da Cunha Júnior <almir.cunha@incognia.com>
2022-08-22 00:22:26 -0300

    Add mean and sd calc of rtt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"math/rand"
 	"net"
 	"os"
@@ -18,6 +19,24 @@ const (
 var (
 	nodes []string
 )
+
+func FindShortestPath(ori string, dest string, encoder *json.Encoder, decoder *json.Decoder) (*ResponsePayload, *time.Duration, error) {
+	requestPayload := RequestPayload{
+		Ori:  ori,
+		Dest: dest,
+	}
+	start := time.Now()
+	if err := encoder.Encode(requestPayload); err != nil {
+		return nil, nil, err
+
+	}
+	responsePayload := ResponsePayload{}
+	if err := decoder.Decode(&responsePayload); err != nil {
+		return nil, nil, err
+	}
+	rtt := time.Since(start) - responsePayload.CalcDuration
+	return &responsePayload, &rtt, nil
+}
 
 func main() {
 	host, ok := os.LookupEnv("HOST")
@@ -40,6 +59,7 @@ func main() {
 	}
 	defer closeTCPConnection(conn)
 
+	var samples []time.Duration
 	decoder := json.NewDecoder(conn)
 	encoder := json.NewEncoder(conn)
 	for i := 0; i < SAMPLES_SIZE; i++ {
@@ -47,24 +67,28 @@ func main() {
 		ori := nodes[rand.Intn(len(nodes))]
 		dest := nodes[rand.Intn(len(nodes))]
 
-		requestPayload := RequestPayload{
-			Ori:  ori,
-			Dest: dest,
-		}
-		log.Printf("sending payload to server %v", requestPayload)
-		start := time.Now()
-		if err := encoder.Encode(requestPayload); err != nil {
-			log.Fatal(err)
-
-		}
-		responsePayload := ResponsePayload{}
-		if err := decoder.Decode(&responsePayload); err != nil {
+		log.Printf("sending request to find the shortest path between %s and %s", ori, dest)
+		res, rtt, err := FindShortestPath(ori, dest, encoder, decoder)
+		if err != nil {
 			log.Fatal(err)
 		}
-		rtt := time.Since(start) - responsePayload.CalcDuration
-
-		log.Printf("payload received from server (RTT %v) %v", rtt, responsePayload)
+		samples = append(samples, *rtt)
+		log.Printf("shortest path received %v", res.Path)
 	}
+
+	var mean float64
+	for _, sample := range samples {
+		mean += float64(sample)
+	}
+	mean = mean / float64(len(samples))
+
+	var sd float64
+	for _, sample := range samples {
+		sd += math.Pow((float64(sample) - mean), 2)
+	}
+	sd = math.Sqrt(sd / float64(len(samples)))
+
+	log.Printf("average RTT is %.2f (+- %.2f)", mean, sd)
 }
 
 func setup() error {
