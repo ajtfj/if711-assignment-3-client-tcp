09f1f954315a5c6059ccdc7ad4af76cb52bf5429
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2022-08-21 22:08:54 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ajtfj/if711-assignment-3-client-tcp
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	SAMPLES_SIZE = 10000
+	NODES_FILE   = "nodes.txt"
+)
+
+var (
+	nodes []string
+)
+
+func main() {
+	host, ok := os.LookupEnv("HOST")
+	if !ok {
+		log.Fatal("undefined PORT")
+	}
+
+	if err := setup(); err != nil {
+		log.Fatal(err)
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closeTCPConnection(conn)
+
+	decoder := json.NewDecoder(conn)
+	encoder := json.NewEncoder(conn)
+	for i := 0; i < SAMPLES_SIZE; i++ {
+		rand.Seed(time.Now().UnixMilli())
+		ori := nodes[rand.Intn(len(nodes))]
+		dest := nodes[rand.Intn(len(nodes))]
+
+		requestPayload := RequestPayload{
+			Ori:  ori,
+			Dest: dest,
+		}
+		if err := encoder.Encode(requestPayload); err != nil {
+			log.Fatal(err)
+		}
+
+		responsePayload := ResponsePayload{}
+		if err := decoder.Decode(&responsePayload); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Shortest path between %s and %s is: %v\n", ori, dest, responsePayload.Path)
+	}
+}
+
+func setup() error {
+	file, err := os.ReadFile(NODES_FILE)
+	if err != nil {
+		return err
+	}
+
+	nodes = strings.Split(string(file), " ")
+
+	return nil
+}
+
+type RequestPayload struct {
+	Ori  string `json:"ori"`
+	Dest string `json:"dest"`
+}
+
+type ResponsePayload struct {
+	Path         []string      `json:"path"`
+	CalcDuration time.Duration `json:"calc-duration"`
+}
+
+type ResponseErrorPayload struct {
+	Message string `json:"message"`
+}
+
+func closeTCPConnection(conn net.Conn) {
+	err := conn.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
